a2283974705f16a5f072ebaac4ee3a7e3348c1c1
issamoxix <issamhaidaoui@gmail.com>
2024-05-11 19:03:26 +0200

    add  tests for the endpoints

--- tests/websocket_test.go
+++ tests/websocket_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"RockPaperScissor/handlers"
 	"RockPaperScissor/pkg"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -11,15 +12,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWebSocketHandler(t *testing.T) {
+func setupRouter() *gin.Engine {
 	hub := pkg.NewHub()
+	hub.Pools["testOk"] = pkg.NewPool()
+	hub.Pools["testFull"] = pkg.NewPool()
+
+	hub.Pools["testFull"].Clients[&pkg.Client{
+		ID: 1,
+	}] = true
+	hub.Pools["testFull"].Clients[&pkg.Client{
+		ID: 2,
+	}] = true
+
 	r := gin.Default()
 	r.GET("/game/:gameId", handlers.HandleWebsocketGame(hub))
+	r.POST("/game", handlers.HandleNewGame(hub))
+	r.GET("/game/:gameId/valid", handlers.HandleValidGame(hub))
+	return r
+}
+
+func TestWebSocketHandler(t *testing.T) {
+
+	r := setupRouter()
 
 	srv := httptest.NewServer(r)
 	defer srv.Close()
 
-	wsURL := "ws" + srv.URL[4:] + "/game/123"
+	wsURL := "ws" + srv.URL[4:] + "/game/testOk"
 
 	ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 	if err != nil {
@@ -28,4 +47,64 @@ func TestWebSocketHandler(t *testing.T) {
 	defer ws.Close()
 
 	assert.NotNil(t, ws)
+
+}
+
+func TestNewGameHandler(t *testing.T) {
+	r := setupRouter()
+	req, err := http.NewRequest("POST", "/game", nil)
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+}
+
+func TestValidGameNotFound(t *testing.T) {
+	r := setupRouter()
+	gameId := "testId"
+	req, err := http.NewRequest("GET", "/game/"+gameId+"/valid", nil)
+
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+
+}
+func TestValidGameFound(t *testing.T) {
+	r := setupRouter()
+	gameId := "testOk"
+	req, err := http.NewRequest("GET", "/game/"+gameId+"/valid", nil)
+
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+}
+func TestValidGameFull(t *testing.T) {
+	r := setupRouter()
+	gameId := "testFull"
+	req, err := http.NewRequest("GET", "/game/"+gameId+"/valid", nil)
+
+	if err != nil {
+		t.Fatalf("failed to create request: %v", err)
+	}
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusForbidden, w.Code)
+
 }
