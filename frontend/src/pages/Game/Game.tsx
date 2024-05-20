455160bece9ed5658b534983506e5ef140611457
Issam Haidaoui <issamhaidaoui@gmail.com>
2024-05-20 14:43:49 +0200

    add assets

--- frontend/src/pages/Game/Game.tsx
+++ frontend/src/pages/Game/Game.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { connect, disconnect } from '../../api';
-import ControllButton from '../../components/ControllButton';
 import Chat from '../../components/ChatComp/ChatComp';
-import { useParams } from 'react-router-dom';
+import { ControllButton } from '../../components/ControllButton';
 
 function App() {
   const [IncommingMsg, setIncommingMsg] = useState("")
